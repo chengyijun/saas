5cc9aec885e38f2a15c296848eeac7e0557f7f28
abel <cyjmmy@foxmail.com>
2022-02-25 16:36:11 +0800

    导航栏默认选中

--- web/templatetags/project.py
+++ web/templatetags/project.py
@@ -1,5 +1,6 @@
 from django.core.handlers.wsgi import WSGIRequest
 from django.template import Library
+from django.urls import reverse
 
 from web.models import Project, ProjectUser
 
@@ -27,3 +28,27 @@ def project_select(request: WSGIRequest):
         current_enter_project_name = current_enter_project.name
     return {"my_projects": my_projects, "join_projects": join_projects,
             "current_enter_project_name": current_enter_project_name}
+
+
+@register.inclusion_tag("inclusion/manage_nav.html")
+def manage_nav(request: WSGIRequest):
+    datas = [
+        {
+            "url": reverse("web:wiki", kwargs={"project_id": request.tracer.current_project.id}),
+            "text": "wiki"
+        },
+        {
+            "url": reverse("web:file", kwargs={"project_id": request.tracer.current_project.id, "parent_id": 0}),
+            "text": "文件"
+        },
+        {
+            "url": reverse("web:issuse", kwargs={"project_id": request.tracer.current_project.id}),
+            "text": "问题"
+        }
+    ]
+
+    for data in datas:
+        url = data.get("url")
+        if url.startswith(request.path_info):
+            data.setdefault("is_active", True)
+    return {"datas": datas}
